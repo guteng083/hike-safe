7310d70b5a0b4e356a0b3b86dc2db16548f65fd6
laksanii <prazetyo111@gmail.com>
2025-01-17 08:59:28 +0700

    first commit

---
+++ src/main/java/com/haven/app/haven/HavenApplication.java
@@ -0,0 +1,13 @@
+package com.haven.app.haven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haven/app/haven/HavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haven.app.haven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
